f632a52a247e637a22e56edae9a3e1fedc6b224d
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-03-22 17:49:31 -0600

    Continuing to work on gold app

--- CS2610_Stewart_Braeden_assn4/settings.py
+++ CS2610_Stewart_Braeden_assn4/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gold.apps.GoldConfig',
     'unitconv.apps.UnitconvConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- CS2610_Stewart_Braeden_assn4/urls.py
+++ CS2610_Stewart_Braeden_assn4/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('gold/', include('gold.urls')),
     path('unitconv/', include('unitconv.urls')),
     path('admin/', admin.site.urls),
 ]
