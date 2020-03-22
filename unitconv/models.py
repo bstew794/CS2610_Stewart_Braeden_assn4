4f0099ba26661f21b80cb47d6f8a3a64694685a5
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-03-22 17:01:22 -0600

    Implemented conversion factors for convert view

--- unitconv/models.py
+++ unitconv/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ConversionFactor(models.Model):
+    g = 1.0
+    T = 907185.0 * g
+    t_oz = 31.1 * g
+    kg = 1000.0 * g
+    lb = 453.6 * g
+    oz = 28.35 * g
--- unitconv/views.py
+++ unitconv/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse, HttpResponse
+from .models import ConversionFactor
 
 
 def convert(request):
     sentFrom = request.GET['from']
     to = request.GET['to']
-    value = request.GET['value']
+    value = float(request.GET['value'])
 
-    response = [
-        {'from': sentFrom},
-        {'to': to},
-        {'value': value}
-    ]
+    factor = get_object_or_404(ConversionFactor, pk=1)
 
-    return JsonResponse(response, safe=False)
+    try:
+        value = (value * float(getattr(factor, sentFrom))) / float(getattr(factor, to))
+
+        response = {
+            "units": to,
+            "value": value
+        }
+
+        return JsonResponse(response)
+
+    except AttributeError as error:
+        response = {'error': 'Invalid unit conversion request'}
+        print(error)
+
+        return JsonResponse(response)
