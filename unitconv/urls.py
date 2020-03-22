988369b44a08b235ce8670b9a3f98a0888d43aa8
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-03-22 14:50:07 -0600

    just got JSONResponse() to work

---
+++ unitconv/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'unitconv'
+urlpatterns = [
+    path('convert/', views.convert, name='convert'),
+]
--- unitconv/views.py
+++ unitconv/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def convert(request):
+    sentFrom = request.GET['from']
+    to = request.GET['to']
+    value = request.GET['value']
+
+    response = [
+        {'from': sentFrom},
+        {'to': to},
+        {'value': value}
+    ]
+
+    return JsonResponse(response, safe=False)
